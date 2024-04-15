aa59626da7e80ba80e0a8367f006625a60564b9f
Alessandro Volpicella <alessandro.volpicella@gmail.com>
2024-04-15 15:31:25 +0200

    added authenticator

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,11 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization([a.allow.public()]),
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization([a.allow.public()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -11,7 +13,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
+    defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
